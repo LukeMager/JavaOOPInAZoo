51b20b83d333498a0e472ac930e2283f38ea7af4
Luke Mager <lukemager@students.berkeley.net>
2019-02-14 14:13:10 -0800

    Done with Java OOP In a Zoo

---
+++ src/Animal.java
@@ -0,0 +1,25 @@
+class Animal {
+    public String name;
+    public String favoriteFood;
+    static int population;
+
+    public Animal(String name, String favoriteFood) {
+        this.name = name;
+        this.favoriteFood = favoriteFood;
+        population++;
+    }
+
+    public void sleep() {
+        System.out.println(this.name + " sleeps for 8 hours");
+    }
+
+    public void eat(String food) {
+        System.out.println(this.name + " eats " + food);
+        if(food.equals(this.favoriteFood)){
+            System.out.println("YUM!!! " + this.name + " wants more " + food);
+        } else {
+            sleep();
+        }
+    }
+}
+
